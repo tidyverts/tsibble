dd8b57a6016f5b161a8a21004c3100720918c493
earowang <earo.wang@gmail.com>
2017-09-25 23:20:05 +1000

    simplify flatten_nest code

--- R/parse-key.R
+++ R/parse-key.R
@@ -49,13 +49,10 @@ parse_key <- function(lst_keys) {
 
 # interpret a nested calls A | B | C
 flatten_nest <- function(key) { # call
-  if (is.list(key) && has_length(key, 2)) {
-    return(key)
-  }
-  if (has_length(key, 2) && key[[1]] != sym("-")) {
+  if (!is.list(key) && has_length(key, 2) && key[[1]] != sym("-")) {
     return(flatten_nest(key[[2]]))
   }
-  if (length(key) < 3) 
+  if ((is.list(key) && has_length(key, 2)) || length(key) < 3) 
     return(key)
   op <- key[[1]]
   x <- key[[2]]
