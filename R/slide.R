e89b42105b241de73710a5f033d7026d12061f92
earowang <earo.wang@gmail.com>
2017-10-20 13:43:38 +1100

    fixed slide results for different methods

--- R/slide.R
+++ R/slide.R
@@ -16,6 +16,9 @@
 #' slide(x, sum, size = 3, fill = 0)
 #' slide(x, ~ mean(.), size = 2)
 slide <- function(x, .f, ..., size = 1, fill) {
+  if (size < 1) {
+    abort("The window size must be a positive integer.")
+  }
   UseMethod("slide")
 }
 
@@ -24,7 +27,7 @@ slide <- function(x, .f, ..., size = 1, fill) {
 slide.numeric <- function(x, .f, ..., size = 1, fill = NA_real_) {
   rep_x <- rep_len(list(x), length(x) - (size - 1))
   lst_x <- purrr::imap(rep_x, ~ .x[.y:(.y + size - 1)])
-  c(rep_len(fill, size - 1), purrr::map_dbl(lst_x, .f, ...))
+  c(rep_len(fill, size - 1), purrr::map_dbl(lst_x, .f, ..., .default = fill))
 }
 
 #' @rdname slide
@@ -33,7 +36,7 @@ slide.list <- function(x, .f, ..., size = 1, fill = list()) {
   lst_idx <- seq_len(length(x) - size + 1)
   lst_x <- purrr::map(lst_idx, ~ x[(.):(. + size - 1), , drop = FALSE])
   result <- purrr::map(lst_x, .f, ..., .default = fill)
-  c(rep_len(fill, size - 1), result)
+  c(replicate(n = size - 1, fill), result)
 }
 
 #' @rdname slide
@@ -42,5 +45,5 @@ slide.data.frame <- function(x, .f, ..., size = 1, fill = data.frame()) {
   lst_idx <- seq_len(nrow(x) - size + 1)
   lst_x <- purrr::map(lst_idx, ~ x[(.):(. + size - 1), , drop = FALSE])
   result <- purrr::map(lst_x, .f, ..., .default = fill)
-  c(rep_len(fill, size - 1), result)
+  c(replicate(n = size - 1, fill), result)
 }
