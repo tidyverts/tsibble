ae030ad6ae103da7e2c1da9ae0155e08c151e8de
earowang <earo.wang@gmail.com>
2017-10-29 12:03:37 +1100

    added stretch()

--- R/slide.R
+++ R/slide.R
@@ -44,12 +44,7 @@ slide.data.frame <- function(
 #' @rdname slide
 #' @export
 slider <- function(x, size = 1) {
-  if (is_bare_list(x)) {
-    abort("Unsupported input class: list")
-  }
-  if (!is_bare_numeric(size, n = 1) || size < 1) {
-    abort("The window size must be a positive integer.")
-  }
+  bad_window_function(x, size)
   if (is.data.frame(x)) {
     len_x <- nrow(x)
   } else {
@@ -99,12 +94,7 @@ tile.data.frame <- function(x, .f, ..., size = 1, deframe = TRUE) {
 #' @rdname tile
 #' @export
 tiler <- function(x, size = 1) {
-  if (is_bare_list(x)) {
-    abort("Unsupported input class: list")
-  }
-  if (!is_bare_numeric(size, n = 1) || size < 1) {
-    abort("The window size must be a positive integer.")
-  }
+  bad_window_function(x, size)
   if (is.data.frame(x)) {
     seq_x <- nrow(x)
     denom <- seq_x + 1
@@ -114,4 +104,73 @@ tiler <- function(x, size = 1) {
   }
   frac <- ceiling((seq_x %% denom) / size)
   unname(split(x, frac))
+}
+
+#' Stretching window function
+#'
+#' @param x A vector of numerics, or data frame.
+#' @param .f A function, or formula.
+#' @param ... Additional arguments passed on to `.f`.
+#' @param size Window size.
+#' @param init Initial window size.
+#'
+#' @rdname stretch
+#' @export
+#'
+#' @examples
+#' stretch(1:10, mean, init = 3)
+stretch <- function(x, .f, ..., size = 1, init = 1) {
+  UseMethod("stretch")
+}
+
+#' @rdname stretch
+#' @export
+stretch.numeric <- function(x, .f, ..., size = 1, init = 1) {
+  lst_x <- stretcher(x, size = size, init = init)
+  purrr::map_dbl(lst_x, .f, ..., .default = NA_real_)
+}
+
+#' @rdname stretch
+#' @param deframe TRUE a list is returned. FALSE returns a data frame.
+#' @export
+stretch.data.frame <- function(x, .f, ..., size = 1, init = 1, deframe = TRUE) {
+  lst_x <- stretcher(x, size = size, init = init)
+  if (deframe) {
+    return(purrr::map(lst_x, .f, ...))
+  }
+  purrr::map_df(lst_x, .f, ...)
+}
+
+#' @rdname stretch
+#' @export
+stretcher <- function(x, size = 1, init = 1) {
+  bad_window_function(x, size)
+  if (!is_bare_numeric(init, n = 1) || init < 1) {
+    abort("The initial window must be a positive integer.")
+  }
+  incr <- function(init, size) {
+    init
+    function() {
+      init <<- init + size
+      init
+    }
+  }
+  counter <- incr(init = init, size = size) 
+
+  ncall <- seq_len(ceiling((length(x) - init) / size) - 1)
+  incr_lst <- c(
+    list(seq_len(init)), 
+    purrr::map(ncall, ~ seq_len(counter())),
+    list(seq_along(x))
+  )
+  purrr::map(incr_lst, function(i) x[i])
+}
+
+bad_window_function <- function(x, size) {
+  if (is_bare_list(x)) {
+    abort("Unsupported input class: list")
+  }
+  if (!is_bare_numeric(size, n = 1) || size < 1) {
+    abort("The window size must be a positive integer.")
+  }
 }
