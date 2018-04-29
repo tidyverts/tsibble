3020d1d0e2cf8e9649544a1c425716393bd242b1
earowang <earo.wang@gmail.com>
2018-04-29 16:38:18 +1000

    fixed the display of keys in the header

--- R/type-sum.R
+++ R/type-sum.R
@@ -23,7 +23,7 @@ tbl_sum.tbl_ts <- function(x) {
   n_keys <- big_mark(n_keys(x))
   c(
     first,
-    "Keys" = paste(paste_comma(key_vars(x)), surround(n_keys, "["))
+    "Keys" = paste(paste_comma(format(key(x))), surround(n_keys, "["))
   )
 }
 
