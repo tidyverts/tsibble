ea1e9a43bf2bfca0d534fa8ac6529ede603315db
earowang <earo.wang@gmail.com>
2018-05-21 20:37:02 +1000

    added *_join() for "lst_ts"

--- R/tidyr-verbs.R
+++ R/tidyr-verbs.R
@@ -217,3 +217,21 @@ slice.lst_ts <- mutate.lst_ts
 
 #' @export
 group_by.lst_ts <- mutate.lst_ts
+
+#' @export
+left_join.lst_ts <- mutate.lst_ts
+
+#' @export
+right_join.lst_ts <- mutate.lst_ts
+
+#' @export
+full_join.lst_ts <- mutate.lst_ts
+
+#' @export
+inner_join.lst_ts <- mutate.lst_ts
+
+#' @export
+anti_join.lst_ts <- mutate.lst_ts
+
+#' @export
+semi_join.lst_ts <- mutate.lst_ts
