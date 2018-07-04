85ea6883620139360ae4c2b9cd3787b4d26e6149
earowang <earo.wang@gmail.com>
2018-07-04 10:04:29 +1000

    added pslider(); much better structured #38

--- R/slide.R
+++ R/slide.R
@@ -22,7 +22,7 @@ replace_fn_names <- function(fn, replace = list()){
 #' arguments as `slide()`, but return vectors of the corresponding type.
 #' * `slide_dfr()` `slide_dfc()` return data frames using row-binding & column-binding.
 #'
-#' @param .x An atomic vector. Instead [lslide] takes list & data.frame.
+#' @param .x An object to slide over.
 #' @inheritParams purrr::map
 #' @param .size An integer for window size.
 #' @param .fill A single value or data frame to replace `NA`.
@@ -39,15 +39,14 @@ replace_fn_names <- function(fn, replace = list()){
 #' [RcppRoll](https://CRAN.R-project.org/package=RcppRoll) for faster performance.
 #'
 #' @examples
-#' # sliding through a vector ----
-#' x <- 1:10
-#' slide_dbl(x, mean, .size = 3)
-#' slide_dbl(x, mean, .size = 3, .fill = 0)
-#' slide_lgl(x, ~ mean(.) > 1, .size = 3)
-#' @export
+#' .x <- 1:5
+#' .lst <- list(x = .x, y = 6:10, z = 11:15)
+#' slide_dbl(.x, mean, .size = 2)
+#' slide_lgl(.x, ~ mean(.) > 2, .size = 2)
+#' slide(.lst, ~ ., .size = 2)
 slide <- function(.x, .f, ..., .size = 1, .fill = NA) {
   if (is_list(.x)) {
-    lst_x <- lslider(.x, .size = .size) %>% 
+    lst_x <- slider(.x, .size = .size) %>% 
       purrr::modify_depth(1, unlist2)
   } else {
     lst_x <- slider(.x, .size = .size)
@@ -88,7 +87,7 @@ slide_dfc <- function(.x, .f, ..., .size = 1, .fill = NA) {
 #' arguments as `slide2()`, but return vectors of the corresponding type.
 #' * `slide2_dfr()` `slide2_dfc()` return data frames using row-binding & column-binding.
 #'
-#' @param .x,.y An atomic vector.
+#' @param .x,.y Objects to slide over simultaneously.
 #' @inheritParams slide
 #'
 #' @rdname slide2
@@ -100,18 +99,22 @@ slide_dfc <- function(.x, .f, ..., .size = 1, .fill = NA) {
 #'
 #' @export
 #' @examples
-#' x <- 1:10
-#' y <- 10:1
-#' z <- x * 2
-#' slide2_dbl(x, y, cor, .size = 3)
-#' slide2(x, y, cor, .size = 3)
-#' pslide_dbl(list(x, y, z), sum, .size = 3)
+#' .x <- 1:5
+#' .y <- 6:10
+#' .z <- 11:15
+#' .lst <- list(x = .x, y = .y, z = .z)
+#' .df <- as.data.frame(.lst)
+#' slide2(.x, .y, sum, .size = 2)
+#' slide2(.lst, .lst, sum, .size = 2)
+#' slide2(.df, .df, sum, .size = 2)
+#' pslide(.lst, sum, size = 1)
+#' pslide(list(.lst, .lst), ~ ., .size = 2)
 slide2 <- function(.x, .y, .f, ..., .size = 1, .fill = NA) {
   if (is_list(.x)) {
-    lst <- lslider(list(.x, .y), .size = .size) %>% 
+    lst <- pslider(.x, .y, .size = .size) %>% 
       purrr::modify_depth(2, unlist2)
   } else {
-    lst <- slider(list(.x, .y), .size = .size)
+    lst <- pslider(.x, .y, .size = .size)
   }
   c(rep_len(.fill, .size - 1), purrr::map2(lst[[1]], lst[[2]], .f = .f, ...))
 }
@@ -149,11 +152,12 @@ pslide <- function(.l, .f, ..., .size = 1, .fill = NA) {
     .l <- as.list(.l)
   }
   depth <- purrr::vec_depth(.l)
-  if (depth == 2) {
-    lst <- slider(.l, .size = .size)
-  } else if (depth == 3) {
-    lst <- lslider(.l, .size = .size) %>% 
+  if (depth == 2) { # a list of multiple elements
+    lst <- pslider(!!! .l, .size = .size) %>%  # slide simultaneously
       purrr::modify_depth(2, unlist2)
+  } else if (depth == 3) { # a list of lists
+    lst <- pslider(!!! .l, .size = .size) %>% 
+      purrr::modify_depth(3, unlist2)
   }
   c(rep_len(.fill, .size - 1), purrr::pmap(lst, .f, ...))
 }
@@ -200,34 +204,43 @@ pslide_dfc <- function(.l, .f, ..., .size = 1, .fill = NA) {
   new_data_frame(out, .size, .fill = .fill, byrow = FALSE)
 }
 
-#' Splits the input to a list according to the rolling window .size.
+#' Splits the input to a list according to the rolling window size.
 #'
-#' @param ... Objects to be splitted.
+#' @param x An objects to be splitted.
+#' @param ... Multiple objects to be splitted in parallel.
 #' @inheritParams slide
 #' @rdname slider
 #' @export
 #' @examples
-#' x <- 1:10
-#' slider(x, .size = 3)
-#' y <- 10:1
-#' slider(x, y, .size = 3)
+#' .x <- 1:5
+#' .y <- 6:10
+#' .z <- 11:15
+#' .lst <- list(x = .x, y = .y, z = .z)
+#' .df <- as.data.frame(.lst)
+#'
+#' slider(.x, .size = 2)
+#' slider(.lst, .size = 2)
+#' pslider(.lst, .size = 2)
+#' pslider(list(.x, .y), list(.y))
+#' slider(.df, .size = 2)
+#' pslider(.df, .df, .size = 2)
 slider <- function(.x, .size = 1) {
   if (is_atomic(.x)) {
     return(slider_base(.x, .size = .size))
   } else {
-    return(purrr::map(.x, ~ slider_base(., .size = .size)))
+    if (is.data.frame(.x)) .x <- as.list(.x)
+    return(slider_base(.x, .size = .size))
   }
 }
 
-# if .x is a list, slide over elements with this list
-# if .x is a list of lists, slide over each nested list simultaneously
-lslider <- function(.x, .size = 1) {
+#' @rdname slider
+#' @export
+pslider <- function(..., .size = 1) { # parallel sliding
+  # if ... is a data frame, list2() converts it to a list already
+  .x <- list2(...)
   depth <- purrr::vec_depth(.x)
-  if (depth == 1) {
-    return(slider(.x, .size = .size))
-  } else if (depth == 2) { # a list
-    if (is.data.frame(.x)) .x <- as.list(.x)
-    return(slider_base(.x, .size = .size))
+  if (depth == 2) {
+    return(purrr::map(.x, ~ slider_base(., .size = .size)))
   } else if (depth == 3) { # a list of lists
     df_lgl <- purrr::map_lgl(.x, is.data.frame)
     if (any(df_lgl)) {
@@ -235,7 +248,7 @@ lslider <- function(.x, .size = 1) {
     }
     return(purrr::map(.x, ~ slider_base(., .size = .size)))
   } else {
-    abort(".x` must not be deeper than 3.")
+    abort("Must not be deeper than 3.")
   }
 }
 
@@ -273,7 +286,9 @@ unlist2 <- function(x) {
 new_data_frame <- function(x, .size, .fill = NA, .id = NULL, byrow = TRUE) {
   lst <- new_list_along(x[[.size]])
   lst[] <- .fill
-  lst <- rep_len(list(lst), .size - 1)
+  if (.size > 1) {
+    lst <- rep_len(list(lst), .size - 1)
+  }
   if (byrow) {
     return(dplyr::bind_rows(lst, x[-seq_len(.size - 1)], .id = .id))
   }
