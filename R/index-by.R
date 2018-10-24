e77d39b88ed6c426674615263561b87ed9e51de3
earowang <earo.wang@gmail.com>
2018-10-24 16:09:26 +1100

    fixed ci error

--- R/index-by.R
+++ R/index-by.R
@@ -77,9 +77,9 @@ index_by.tbl_ts <- function(.data, ...) {
   # ungroup() protect the index class
   tbl <- mutate(ungroup(.data), !!! exprs)
   idx2 <- sym(expr_name)
-  build_tsibble_meta(
+  build_tsibble(
     tbl, key = key(.data), index = !! idx, index2 = !! idx2,
-    groups = groups(.data), regular = is_regular(.data),
+    groups = groups(.data), regular = is_regular(.data), validate = FALSE,
     ordered = is_ordered(.data), interval = interval(.data)
   )
 }
