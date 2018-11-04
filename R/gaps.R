d4369c0b63cfe85c2a8c662d8ce4e0467a21092e
earowang <earo.wang@gmail.com>
2018-11-04 12:12:33 +1100

    new vignette on implicit missingness

--- R/gaps.R
+++ R/gaps.R
@@ -27,10 +27,10 @@ count_gaps <- function(.data, ...) {
 #'   stop("Please install the ggplot2 package to run these following examples.")
 #' }
 #' library(ggplot2)
-#' ggplot(ped_gaps, aes(colour = Sensor)) +
-#'   geom_linerange(aes(x = Sensor, ymin = .from, ymax = .to)) +
-#'   geom_point(aes(x = Sensor, y = .from)) +
-#'   geom_point(aes(x = Sensor, y = .to)) +
+#' ggplot(ped_gaps, aes(x = Sensor, colour = Sensor)) +
+#'   geom_linerange(aes(ymin = .from, ymax = .to)) +
+#'   geom_point(aes(y = .from)) +
+#'   geom_point(aes(y = .to)) +
 #'   coord_flip() +
 #'   theme(legend.position = "bottom")
 count_gaps.tbl_ts <- function(.data, .full = FALSE, ...) {
