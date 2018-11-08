1eb25eab75bc53e4fc1526c096dfa793ad580c0d
earowang <earo.wang@gmail.com>
2018-11-09 09:48:36 +1100

    soft-deprecated arg `fromLast` in find_duplicates()

--- R/as-tsibble.R
+++ R/as-tsibble.R
@@ -573,17 +573,19 @@ use_id <- function(x, key) {
 #' the helper [id]. If a univariate time series (without an explicit key),
 #' simply call `id()`.
 #' @param index A bare (or unquoted) variable to specify the time index variable.
-#' @param fromLast `TRUE` does the duplication check from the last of identical
+#' @param from_last `TRUE` does the duplication check from the last of identical
 #' elements.
+#' @param fromLast Deprecated. Please use `from_last` instead.
 #'
 #' @return A logical vector of the same length as the row number of `data`
 #' @export
-find_duplicates <- function(data, key = id(), index, fromLast = FALSE) {
+find_duplicates <- function(data, key = id(), index, from_last = FALSE, 
+  fromLast = from_last) {
   key <- use_id(data, !! enquo(key))
   index <- validate_index(data, enquo(index))
 
   grouped_df(data, vars = key) %>%
-    mutate(!! "zzz" := duplicated.default(!! index, fromLast = fromLast)) %>%
+    mutate(!! "zzz" := duplicated.default(!! index, fromLast = from_last)) %>%
     dplyr::pull(!! "zzz")
 }
 
