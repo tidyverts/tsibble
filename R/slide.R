3d81e09f90dfdfa4a7a955d9ad46f3e06c8fc2f0
earowang <earo.wang@gmail.com>
2019-02-13 15:31:36 +1100

    roll_tsibble for grouped_ts in and out

--- R/slide.R
+++ R/slide.R
@@ -428,7 +428,11 @@ roll_tsibble <- function(.x, indices, .id = ".id") {
       indices, 
       ~ purrr::imap(.x, ~ rep.int(.y, length(.x)))
     ), use.names = FALSE)
-  res <- mutate(tbl[row_indices, ], !! .id := id_indices)
+  res <- 
+    group_by(
+      mutate(tbl[row_indices, ], !! .id := id_indices), 
+      !!! groups(.x)
+    )
   new_key <- c(sym(.id), key(.x))
   build_tsibble(
     res, key = new_key, index = !! index(.x), index2 = !! index2(.x),
