9e857a5f03e8f59a00fc7bcb3368344683cf729f
earowang <earo.wang@gmail.com>
2019-05-23 22:28:14 +1000

    rm utils-format.R

--- R/interval.R
+++ R/interval.R
@@ -291,3 +291,38 @@ time_to_date.ts <- function(x, tz = "UTC", ...) {
 time_to_date.gts <- function(x, tz = "UTC", ...) {
   time_to_date(x$bts, tz = tz, ...)
 }
+
+# regular time interval is obtained from the greatest common divisor of positive
+# time distances.
+gcd_interval <- function(x) {
+  if (has_length(x, 1L) || has_length(x, 0L)) { # only one time index
+    0
+  } else if (is_integerish(x)) {
+    gcd_vector(x)
+  } else {
+    gcd_vector_r(unique(round(abs(diff(x)), digits = 6)))
+  }
+}
+
+gcd2 <- function(a, b) {
+  if (isTRUE(all.equal(b, 0))) a else gcd2(b, a %% b)
+}
+
+gcd_vector_r <- function(x) Reduce(gcd2, x)
+
+split_period <- function(x) {
+  output <- lubridate::seconds_to_period(x)
+  list(
+    year = output$year, month = output$month, day = output$day,
+    hour = output$hour, minute = output$minute, second = output$second
+  )
+}
+
+has_tz <- function(x) {
+  tz <- attr(x, "tzone")[[1]]
+  if (is_null(tz) && !inherits(x, "POSIXct")) {
+    FALSE
+  } else {
+    TRUE
+  }
+}
--- R/utils.R
+++ R/utils.R
@@ -1,28 +1,4 @@
-split_period <- function(x) {
-  output <- lubridate::seconds_to_period(x)
-  list(
-    year = output$year, month = output$month, day = output$day,
-    hour = output$hour, minute = output$minute, second = output$second
-  )
-}
-
-# regular time interval is obtained from the greatest common divisor of positive
-# time distances.
-gcd_interval <- function(x) {
-  if (has_length(x, 1L) || has_length(x, 0L)) { # only one time index
-    0
-  } else if (is_integerish(x)) {
-    gcd_vector(x)
-  } else {
-    gcd_vector_r(unique(round(abs(diff(x)), digits = 6)))
-  }
-}
-
-gcd2 <- function(a, b) {
-  if (isTRUE(all.equal(b, 0))) a else gcd2(b, a %% b)
-}
-
-gcd_vector_r <- function(x) Reduce(gcd2, x)
+## helpers
 
 min0 <- function(...) {
   min(..., na.rm = TRUE)
@@ -40,12 +16,61 @@ list_is_named <- function(x) {
   nms <- names(x)
   map_lgl(nms, ~ . != "")
 }
+# ref: tibble:::big_mark
+big_mark <- function(x, ...) {
+  mark <- if (identical(getOption("OutDec"), ",")) "." else ","
+  ret <- formatC(x, big.mark = mark, ...)
+  ret[is.na(x)] <- "??"
+  ret
+}
 
-has_tz <- function(x) {
-  tz <- attr(x, "tzone")[[1]]
-  if (is_null(tz) && !inherits(x, "POSIXct")) {
-    FALSE
+# ref: tibble:::cat_line
+cat_line <- function(...) {
+  cat(paste0(..., "\n"), sep = "")
+}
+
+dim_tbl_ts <- function(x) {
+  dim_x <- dim(x)
+  format_dim <- map_chr(dim_x, big_mark)
+  paste(format_dim, collapse = " x ")
+}
+
+comma <- function(...) {
+  paste(..., collapse = ", ")
+}
+
+backticks <- function(x) {
+  paste0("`", x, "`")
+}
+
+brackets <- function(x) {
+  paste0("[", x, "]")
+}
+
+angle_brackets <- function(x) {
+  paste0("<", x, ">")
+}
+
+# inlined from https://github.com/r-lib/cli/blob/master/R/utf8.R
+is_utf8_output <- function() {
+  opt <- getOption("cli.unicode", NULL)
+  if (! is_null(opt)) {
+    isTRUE(opt)
   } else {
-    TRUE
+    l10n_info()$`UTF-8` && !is_latex_output()
+  }
+}
+
+is_latex_output <- function() {
+  if (!("knitr" %in% loadedNamespaces())) return(FALSE)
+  get("is_latex_output", asNamespace("knitr"))()
+}
+
+format_tz <- function(x) {
+  tz <- attr(x, "tzone")[[1]]
+  if (is_null(tz) || is.character(tz) && !nzchar(tz)) {
+    "?"
+  } else {
+    tz
   }
 }
