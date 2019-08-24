e5d65f9bc21828cb1886456f79d70e6dad503805
earowang <earo.wang@gmail.com>
2019-08-24 16:29:25 +1000

    rm the error msg in tsibble() not TRUE when tibble() supports vctrs

--- R/as-tsibble.R
+++ R/as-tsibble.R
@@ -42,9 +42,6 @@ globalVariables(c(".rows"))
 tsibble <- function(..., key = NULL, index, regular = TRUE, .drop = TRUE) {
   stopifnot(is_logical(regular, n = 1))
   dots <- list2(...)
-  if (has_length(dots, 1) && is.data.frame(dots[[1]])) {
-    abort("Must not be a data frame, do you want `as_tsibble()`?")
-  }
   tbl <- tibble(!!!dots)
   index <- enquo(index)
   build_tsibble(tbl,
