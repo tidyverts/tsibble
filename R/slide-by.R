c2d6659f03ffed8dd24b00b5f9ba6b32d210e510
earowang <earo.wang@gmail.com>
2019-09-11 22:30:23 +1000

    added tbl_sum() for tbl_rts

--- R/slide-by.R
+++ R/slide-by.R
@@ -1,17 +1,23 @@
-slide_by <- function(.data, .size = 1, .step = 1) {
+slide_by <- function(.data, .size = 1, .step = 1, ...) {
   UseMethod("slide_by")
 }
 
-slide_by.tbl_ts <- function(.data, .size = 1, .step = 1) {
+slide_by.tbl_ts <- function(.data, .size = 1, .step = 1, ...) {
   key_data <- key_data(.data)
-  key_data[[".rows"]] <- as_list_of(map(key_rows(.data),
+  lst_rows <- as_list_of(map(key_rows(.data),
     slider2, .size = .size, .step = .step))
-  new_roll_tsibble(.data, key_data = key_data)
+  key_data[[".rows"]] <- lst_rows
+  nfolds <- max(map_int(lst_rows, vec_size))
+  new_rolling_tsibble(.data, key_data = key_data, .nfolds = nfolds)
 }
 
-slide_by.grouped_ts <- function(.data, .size = 1, .step = 1) {
+slide_by.grouped_ts <- function(.data, .size = 1, .step = 1, ...) {
   warn("Ignoring grouping structure.")
   slide_by.tbl_ts(.data, .size, .step)
+}
+
+slide_by.tbl_rts <- function(.data, ...) {
+  .data
 }
 
 slider2 <- function(x, .size = 1, .step = 1) {
@@ -21,7 +27,17 @@ slider2 <- function(x, .size = 1, .step = 1) {
   as_list_of(map(lst_idx, function(idx) x[idx:(idx + .size - 1)]))
 }
 
-new_roll_tsibble <- function(.data, key_data) {
+new_rolling_tsibble <- function(.data, key_data, .nfolds = NULL) {
   attr(.data, "key") <- key_data
-  new_tsibble(.data, class = "tbl_rts")
+  new_tsibble(.data, .nfolds = .nfolds, class = "tbl_rts")
+}
+
+nfolds <- function(.data) {
+  .data %@% .nfolds
+}
+
+tbl_sum.tbl_rts <- function(x) {
+  n_folds <- brackets(big_mark(nfolds(x)))
+  res <- NextMethod()
+  c(res, "Folds" = n_folds)
 }
