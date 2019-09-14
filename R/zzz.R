16c689db7c93886702edab9a8ae90b265c0f66a3
earowang <earo.wang@gmail.com>
2019-09-14 19:57:42 +1000

    used vctrs::s3_register() #120 [ci skip]

--- R/zzz.R
+++ R/zzz.R
@@ -1,43 +1,17 @@
 # nocov start
 .onLoad <- function(...) {
-  register_s3_method("dplyr", "filter", "tbl_ts")
-  register_s3_method("dplyr", "distinct", "tbl_ts")
-  register_s3_method("dplyr", "group_split", "tbl_ts")
-  register_s3_method("dplyr", "group_split", "grouped_ts")
-  register_s3_method("dplyr", "group_trim", "grouped_ts")
+  s3_register("dplyr::filter", "tbl_ts")
+  s3_register("dplyr::distinct", "tbl_ts")
+  s3_register("dplyr::group_split", "tbl_ts")
+  s3_register("dplyr::group_split", "grouped_ts")
+  s3_register("dplyr::group_trim", "grouped_ts")
 
-  register_s3_method("tidyr", "gather", "tbl_ts")
-  register_s3_method("tidyr", "spread", "tbl_ts")
-  register_s3_method("tidyr", "fill", "grouped_ts")
-  register_s3_method("tidyr", "fill", "tbl_ts")
-  register_s3_method("tidyr", "nest", "tbl_ts")
-  register_s3_method("tidyr", "nest", "grouped_ts")
-  register_s3_method("tidyr", "unnest", "tbl_ts")
-
-  invisible()
-}
-
-register_s3_method <- function(pkg, generic, class, fun = NULL) {
-  stopifnot(is.character(pkg), length(pkg) == 1)
-  stopifnot(is.character(generic), length(generic) == 1)
-  stopifnot(is.character(class), length(class) == 1)
-
-  if (is_null(fun)) {
-    fun <- get(paste0(generic, ".", class), envir = parent.frame())
-  } else {
-    stopifnot(is.function(fun))
-  }
-
-  if (pkg %in% loadedNamespaces()) {
-    registerS3method(generic, class, fun, envir = asNamespace(pkg))
-  }
-
-  # Always register hook in case package is later unloaded & reloaded
-  setHook(
-    packageEvent(pkg, "onLoad"),
-    function(...) {
-      registerS3method(generic, class, fun, envir = asNamespace(pkg))
-    }
-  )
+  s3_register("tidyr::gather", "tbl_ts")
+  s3_register("tidyr::spread", "tbl_ts")
+  s3_register("tidyr::fill", "grouped_ts")
+  s3_register("tidyr::fill", "tbl_ts")
+  s3_register("tidyr::nest", "tbl_ts")
+  s3_register("tidyr::nest", "grouped_ts")
+  s3_register("tidyr::unnest", "tbl_ts")
 }
 # nocov end
