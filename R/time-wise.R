53eda978e52016246675532bd16e8b9f0d7fd114
earowang <earo.wang@gmail.com>
2019-11-30 22:49:47 +1100

    tsibble-aware keyed_[lag/lead]() #55

--- R/time-wise.R
+++ R/time-wise.R
@@ -47,3 +47,48 @@ diff_impl <- function(x, lag = 1, differences = 1, fill = NA) {
   diff_x <- diff(x, lag = lag, differences = differences)
   vec_c(vec_repeat(fill, lag * differences), diff_x)
 }
+
+lag_lead <- function(x, n = 1L, default = NA, order_by, op = "+") {
+  if (n < 0 || !is_integerish(n, n = 1)) {
+    abort("`n` must be a non-negative integer.")
+  }
+  if (op == "+") {
+    idx <- vec_match(order_by + n, order_by) # tlead()
+  } else {
+    idx <- vec_match(order_by - n, order_by) # tlag()
+  }
+  idx_na <- vec_in(idx, NA)
+  x <- vec_slice(x, idx)
+  vec_slice(x, idx_na) <- default
+  x
+}
+
+tlag <- function(x, n = 1L, default = NA, order_by) {
+  lag_lead(x, n, default, order_by, "-")
+}
+
+tlead <- function(x, n = 1L, default = NA, order_by) {
+  lag_lead(x, n, default, order_by, "+")
+}
+
+keyed_lag <- function(var, n = 1L, default = NA) {
+  data <- peek_tsibble_mask()
+  col <- sym(names(eval_select(expr({{ var }}), data)))
+  tunits <- default_time_units(interval(data))
+  idx_chr <- index_var(data)
+  grped_df <- new_grouped_df(data, groups = key_data(data))
+  res_df <- mutate(grped_df,
+    !!idx_chr := tlag(!!col, n = n * tunits, default, !!index(data)))
+  res_df[[idx_chr]]
+}
+
+keyed_lead <- function(var, n = 1L, default = NA) {
+  data <- peek_tsibble_mask()
+  col <- sym(names(eval_select(expr({{ var }}), data)))
+  tunits <- default_time_units(interval(data))
+  idx_chr <- index_var(data)
+  grped_df <- new_grouped_df(data, groups = key_data(data))
+  res_df <- mutate(grped_df,
+    !!idx_chr := tlead(!!col, n = n * tunits, default, !!index(data)))
+  res_df[[idx_chr]]
+}
