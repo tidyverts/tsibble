d390948a646be5e4233b6812eb182856f8a0fbcd
earowang <earo.wang@gmail.com>
2020-01-31 09:38:36 +1100

    {slide} -> {slider}

--- R/tile.R
+++ R/tile.R
@@ -4,7 +4,7 @@
 #' \lifecycle{questioning}
 #'
 #' **The rolling window family will be deprecated in the future. Please consider
-#' using the [slide](https://davisvaughan.github.io/slide) package.**
+#' using the [slider](https://davisvaughan.github.io/slider) package.**
 #'
 #' Tiling window without overlapping observations:
 #' * `tile()` always returns a list.
@@ -64,7 +64,7 @@ tile_dfc <- function(.x, .f, ..., .size = 1, .bind = FALSE) {
 #' \lifecycle{questioning}
 #'
 #' **The rolling window family will be deprecated in the future. Please consider
-#' using the [slide](https://davisvaughan.github.io/slide) package.**
+#' using the [slider](https://davisvaughan.github.io/slider) package.**
 #'
 #' Tiling window without overlapping observations:
 #' * `tile2()` and `ptile()` always returns a list.
@@ -220,7 +220,7 @@ tile_tsibble <- function(.x, .size = 1, .id = ".id") {
 #' \lifecycle{questioning}
 #'
 #' **The rolling window family will be deprecated in the future. Please consider
-#' using the [slide](https://davisvaughan.github.io/slide) package.**
+#' using the [slider](https://davisvaughan.github.io/slider) package.**
 #' Multiprocessing equivalents of [slide()], [tile()], [stretch()] prefixed by `future_`.
 #' * Variants for corresponding types: `future_*_lgl()`, `future_*_int()`,
 #' `future_*_dbl()`, `future_*_chr()`, `future_*_dfr()`, `future_*_dfc()`.
