d1a227ae7b59a54745cc95733cc6b290883b2c07
Earo Wang <earo.wang@gmail.com>
2020-02-20 14:49:41 +1300

    rm EuStockMarkets example to fix checks

--- R/tsibble2ts.R
+++ R/tsibble2ts.R
@@ -17,10 +17,6 @@
 #' # a monthly series
 #' x1 <- as_tsibble(AirPassengers)
 #' as.ts(x1)
-#'
-#' # equally spaced over trading days, not smart enough to guess frequency
-#' x2 <- as_tsibble(EuStockMarkets)
-#' head(as.ts(x2, frequency = 260))
 as.ts.tbl_ts <- function(x, value, frequency = NULL, fill = NA_real_, ...) {
   stopifnot(!is_null(fill))
   value <- enquo(value)
