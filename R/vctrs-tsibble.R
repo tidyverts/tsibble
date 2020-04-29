f01835896a376cd4c703d8377f5e992195101ae1
earowang <earo.wang@gmail.com>
2020-04-29 20:57:40 +1200

    vec_cast and vec_ptype2 for tsibble

--- R/vctrs-tsibble.R
+++ R/vctrs-tsibble.R
@@ -1,21 +1,25 @@
-#' @rdname vctrs-compat
-#' @keywords internal
-#' @method vec_ptype2 tbl_ts
 #' @export
-#' @export vec_ptype2.tbl_ts
 vec_ptype2.tbl_ts <- function(x, y, ...) {
   UseMethod("vec_ptype2.tbl_ts", y)
 }
 
-#' @method vec_ptype2.tbl_ts tbl_ts
 #' @export
 vec_ptype2.tbl_ts.tbl_ts <- function(x, y, ...) {
-  tsibble_ptype2(x, y)
+  tsibble_ptype2(x, y, ...)
 }
 
-vec_ptype2.tbl_ts.data.frame <- function(x, y, ...) {
-  
+#' @export
+vec_ptype2.data.frame.tbl_ts <- function(x, y, ...) {
+  vctrs:::df_ptype2(x, y)
 }
+
+#' @export
+vec_ptype2.tbl_df.tbl_ts <- function(x, y, ...) {
+  vctrs:::tib_ptype2(x, y)
+}
+
+#' @export
+vec_ptype2.tbl_ts.tbl_df <- vec_ptype2.tbl_ts.tbl_ts
 
 vec_ptype2.grouped_ts.tbl_ts <- function(x, y, ...) {
   
@@ -38,7 +42,7 @@ tsibble_ptype2 <- function(x, y, ...) {
     }
     key_x <- union(key_x, key_vars(y))
   }
-  lst <- vctrs:::tib_ptype2(x, y)
+  lst <- vctrs:::tib_ptype2(x, y, ...)
   build_tsibble_meta(lst, key_data = new_key_data(lst[key_x]),
     index = idx_x, index2 = idx_x, ordered = TRUE,
     interval = new_interval())
@@ -48,17 +52,35 @@ new_key_data <- function(x) {
   tibble(!!!x, !!".rows" := list_of(.ptype = integer()))
 }
 
-#' @rdname vctrs-compat
-#' @keywords internal
-#' @method vec_cast tbl_ts
 #' @export
-#' @export vec_cast.tbl_ts
 vec_cast.tbl_ts <- function(x, to, ...) {
   UseMethod("vec_cast.tbl_ts")
 }
 
-#' @method vec_cast.tbl_ts tbl_ts
 #' @export
 vec_cast.tbl_ts.tbl_ts <- function(x, to, ...) {
-  x
+  is_identical <- identical(x, to)
+  tbl <- vctrs:::tib_cast(x, to, ...)
+  build_tsibble(tbl,
+    key = key_vars(to),
+    key_data = if (is_identical) key_data(x) else NULL,
+    index = index_var(to), index2 = index2_var(to),
+    ordered = is_ordered(to),
+    validate = FALSE, .drop = key_drop_default(to))
+}
+
+#' @export
+vec_cast.tbl_ts.tbl_df <- vec_cast.tbl_ts.tbl_ts
+
+#' @export
+vec_cast.tbl_ts.data.frame.tbl_ts <- vec_cast.tbl_ts.tbl_ts
+
+#' @export
+vec_cast.tbl_df.tbl_ts <- function(x, to, ...) {
+  vctrs:::tib_cast(x, to, ...)
+}
+
+#' @export
+vec_cast.data.frame.tbl_ts <- function(x, to, ...) {
+  vctrs:::df_cast(x, to, ...)
 }
