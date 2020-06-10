62ed8778257dc4531ab515161232ed83c0ac984b
earowang <earo.wang@gmail.com>
2020-06-10 17:06:32 +1200

    fixed new_data() for grouped_ts closes #193

--- R/new-data.R
+++ R/new-data.R
@@ -54,7 +54,7 @@ new_data.tbl_ts <- function(.data, n = 1L, keep_all = FALSE, ...) {
 
   out <- unwrap(ungroup(new_lst), .col = !!idx)
   if (keep_all) {
-    out <- vec_rbind(new_data_frame(.data[0L, ]), out)
+    out <- vec_rbind(grped_df[0L, ], out)
   } else { # reorder column names according to the data input
     out <- out[setdiff(names(.data), measured_vars(.data))]
   }
