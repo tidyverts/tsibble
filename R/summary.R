ea6d3cee815cf69993c7607f4fc9cafec2cf4c10
Rob J Hyndman <robjhyndman@gmail.com>
2026-02-09 12:39:41 +1100

    Added summary methods for yearmonth and yearquarter (#319)
    * Added summary methods for yearmonth and yearquarter
    
    * Add summary.yearweek
    
    * Update docs
    
    ---------
    
    Co-authored-by: Mitchell O'Hara-Wild <mail@mitchelloharawild.com>

---
+++ R/summary.R
@@ -0,0 +1,52 @@
+#' @export
+summary.yearquarter <- function(object, digits = 12L, ...) {
+  x <- summary.default(unclass(object), digits = digits, ...)
+  if (m <- match("NA's", names(x), 0L)) {
+    NAs <- as.integer(x[m])
+    x <- x[-m]
+    attr(x, "NAs") <- NAs
+  }
+  output <- yearquarter(.Date(unclass(x)))
+  attributes(output) <- attributes(x)
+  class(output) <- c("summarycal", class(object))
+  output
+}
+
+#' @export
+summary.yearmonth <- function(object, digits = 12L, ...) {
+  x <- summary.default(unclass(object), digits = digits, ...)
+  if (m <- match("NA's", names(x), 0L)) {
+    NAs <- as.integer(x[m])
+    x <- x[-m]
+    attr(x, "NAs") <- NAs
+  }
+  output <- yearmonth(.Date(unclass(x)))
+  attributes(output) <- attributes(x)
+  class(output) <- c("summarycal", class(object))
+  output
+}
+
+#' @export
+summary.yearweek <- function(object, digits = 12L, ...) {
+  x <- summary.default(unclass(object), digits = digits, ...)
+  if (m <- match("NA's", names(x), 0L)) {
+    NAs <- as.integer(x[m])
+    x <- x[-m]
+    attr(x, "NAs") <- NAs
+  }
+  output <- yearweek(.Date(unclass(x)))
+  attributes(output) <- attributes(x)
+  class(output) <- c("summarycal", class(object))
+  output
+}
+
+#' @export
+format.summarycal <- function(x, ...) {
+  xx <- x
+  class(xx) <- class(x)[-1]
+  xx <- c(
+    format(xx),
+    `NA's` = if (length(a <- attr(x, "NAs"))) as.character(a)
+  )
+  xx
+}
